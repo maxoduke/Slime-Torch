918945b36645703c5e7c1fa60b98bdd4f75b77fc
Maxoduke <maxoduke@outlook.com>
2024-04-08 16:02:02 +0530

    Formatting and optimization

--- common/src/main/java/dev/maxoduke/mods/slimetorch/SlimeTorch.java
+++ common/src/main/java/dev/maxoduke/mods/slimetorch/SlimeTorch.java
@@ -1,8 +1,6 @@
 package dev.maxoduke.mods.slimetorch;
 
 import net.minecraft.world.item.Item;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
 
@@ -10,8 +8,6 @@ public class SlimeTorch
 {
     public static final String MOD_ID = "slimetorch";
     public static final String ITEM_NAME = "slime_torch";
-    public static final String MOD_NAME = "Slime Torch";
-    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 
     public static final Item ITEM = new Item(new Item.Properties());
 
--- common/src/main/java/dev/maxoduke/mods/slimetorch/SlimeTorchItemPropertyFunction.java
+++ common/src/main/java/dev/maxoduke/mods/slimetorch/SlimeTorchItemPropertyFunction.java
@@ -4,8 +4,6 @@ import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.item.ClampedItemPropertyFunction;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.item.ItemEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.levelgen.WorldgenRandom;
